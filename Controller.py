379983eacaad4cc0721bf7628877790e3d9a091f
Felipe Lupoli <felipefplupoli@gmail.com>
2026-07-23 16:19:33 -0400

    mexendo na view

---
+++ Controller.py
@@ -0,0 +1,16 @@
+from models import *
+from dao import *
+
+class pessoaController:
+    @staticmethod
+    def cadastra(nome,email):
+        pessoa = Pessoa(nome,email)
+        pessoaDao.cadastrar(pessoa)
+
+
+class usuarioController:
+    @staticmethod
+    def cadastra_usuario(email,login,senha):
+        pessoa = Pessoa(None,email)
+        usuario = Usuario(pessoa,login,senha)
+        usuarioDao.cadastrar(usuario)
---
+++ view.py
@@ -0,0 +1,26 @@
+from models import *
+from dao import *
+from Controller import *
+
+print('-----MENU------')
+print('1. Criar o seu cadastro')
+print('2. Criar o seu usuário')
+print('3. Apagar cadastro')
+print('4. Apagar usuário')
+print('5. Consultar seus logins')
+print('6. Sair')
+while True:
+    x =  int(input("Insira o valor :"))
+    if x == 1:
+        nome = input("Digite o seu nome :")
+        email = input("Digite o seu email :")
+        pessoaController.cadastra(nome,email)
+
+    if x == 2:
+        email2 = input("Digite seu email cadastrado :")
+        login = input("Digite o seu login :")
+        senha = input("Insira a senha :")
+        usuarioController.cadastra_usuario(email2,login,senha)
+    if x == 6:
+        break
+    
